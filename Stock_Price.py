1d1b6dd6c1358b525bf5a9100e8a8397793b50de
SunnyShilongo <112655746+SunnyShilongo@users.noreply.github.com>
2024-02-18 23:50:33 +0200

    Numpy Creating numpy arrays using np.array(my_list) np.arange to give inputs in terms of start, stop, skze, np.reshape Indexing and Selection Broadcasting, Slicing, 2Dimensional Array, Conditional Selection

---
+++ Stock_Price.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write(""""
+#Simple Stock Price Application
+
+
+#Shown are the stock closing prices and volume of Google""")
+
+#Define the ticker symbol
+tickerSymbol = "GOOGL"
+
+#Get data on the ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#Get the historical prices for this ticker
+tickerDF = tickerData.history(period="id", start="2010-5-31", end="2020-5-31")
+
+#Open high, Low Close Volume Dividends Stock Splits
+
+st.line_chart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
