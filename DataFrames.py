3ce49589bc84568cf17a3f2e1b563a7d7ef8379b
SunnyShilongo <112655746+SunnyShilongo@users.noreply.github.com>
2024-03-09 10:02:06 +0200

    Merge remote-tracking branch 'origin/DataFrames' into DataFrames
    # Conflicts:
    #	DataFrames.py

--- DataFrames.py
+++ DataFrames.py
@@ -24,62 +24,86 @@ tips1 = pd.read_csv("C:\\Users\\inoti\\OneDrive\\Documents\\Python for ML and Da
 print(tips1)
 print(tips)
 
-print(tips.head())
-print(tips1.tail())
+# print(tips.head())
+# print(tips1.tail())
+#
+# print('This in Column')
+# print(tips.columns) #Gives a list of the columns in the DF
+# print('This in index')
+# print(tips.index)# GIves the start and the Stop as well as the skze
+#
+# print('This in info')
+# print(tips.info()) # INFORMATION ON DF
+# print('This in describe')
+# print(tips.describe()) # Gives statistical information on DATA in DF. Only does this on the numeric columns, bills but not name column
+#
+# print(tips.head())
+#
+#
+# ''''Columns'''
+# #Calling one column is just the DataFrame name plus the column in square brackets
+# print(tips['total_bill'])
+#
+# #Calling multiple columns at once. Remember the double square brackets
+# print(tips[['total_bill', 'tip', 'Payer Name']])
+#
+# #Arthemic calculation can be done on Columns by calling the DataFrame and the columns to be used
+# #e.g. Total Percentage of the tip to the total bill
+# print(100 * tips['tip'] / tips['total_bill'])
+#
+# #Adding a new column, reference the new column as an preexsisting column and it will be add to the end
+# #NB If you make a new column and the Column name already exists, the data in the existing column will be overwritten
+# tips['Tip and Bill'] = tips['total_bill'] + tips['tip']
+# #OR
+# tips['Tip_Percentage'] = 100 * tips['tip'] / tips['total_bill']
+# print(tips.head())
+#
+# #Rounding by 2 decimal places. Use numpy np.round, then calculation and add the number of decimals at the end
+# tips['Tip and Bill'] = np.round(tips['total_bill'] + tips['tip'],2)
+# tips['Tip_Percentage'] = np.round(100 * tips['tip'] / tips['total_bill'],3)
+# print(tips.head())
+#
+# #Dropping a column is done with .drop() DO NOT forget axis. 0 is Row, 1 is column
+# #NB Remember it is possible that you drop a column but didn't make it permenant to the column
+# print(tips.drop('Tip and Bill', axis = 1)) # Not Permenant
+# tips = tips.drop('Tip and Bill', axis = 1) # Permenant
+# print(tips)
+#
+# '''ROWS'''
+# #Changing Index(Unique Identifier/Primary Key)
+# #print(df.index())
+# print(tips.index)
+# print(tips.set_index("Payment ID"))
+#
+# #Make it Permanent
+# tips = tips.set_index('Payment ID')
+#
+# print(tips.head)
+#
+# #Reset the Index
+# tips = tips.reset_index()
+# print(tips.head)
 
-print('This in Column')
-print(tips.columns) #Gives a list of the columns in the DF
-print('This in index')
-print(tips.index)# GIves the start and the Stop as well as the skze
 
-print('This in info')
-print(tips.info()) # INFORMATION ON DF
-print('This in describe')
-print(tips.describe()) # Gives statistical information on DATA in DF. Only does this on the numeric columns, bills but not name column
+''''FILTERING'''
+#Filtering to confirm which total_bill is greater than 40 dollars
+bool_series = tips['total_bill'] > 40
 
-print(tips.head())
+#Printing only the rows with total_bill higher than 40
+print(tips[bool_series])
 
+#To only show the Male customers
+print(tips[tips['size'] > 3])
 
-''''Columns'''
-#Calling one column is just the DataFrame name plus the column in square brackets
-print(tips['total_bill'])
+''''CONDITIONAL FILTERING'''
+#AND & --- Both need to be True
+#OR |(Plumb_line) --- Either one should be True
+print(tips[(tips['total_bill'] > 30) & (tips['sex'] == 'Male')]) #Both are True
+print(tips[(tips['total_bill'] > 30) | (tips['sex'] == 'Male')]) #One is True
 
-#Calling multiple columns at once. Remember the double square brackets
-print(tips[['total_bill', 'tip', 'Payer Name']])
+#Getting all Orders made on the Weekend including Friday (Long Version)
+print(tips[(tips['day']=='Sun') | (tips['day']=='Sat') | (tips['day']=='Fri')])
 
-#Arthemic calculation can be done on Columns by calling the DataFrame and the columns to be used
-#e.g. Total Percentage of the tip to the total bill
-print(100 * tips['tip'] / tips['total_bill'])
-
-#Adding a new column, reference the new column as an preexsisting column and it will be add to the end
-#NB If you make a new column and the Column name already exists, the data in the existing column will be overwritten
-tips['Tip and Bill'] = tips['total_bill'] + tips['tip']
-#OR
-tips['Tip_Percentage'] = 100 * tips['tip'] / tips['total_bill']
-print(tips.head())
-
-#Rounding by 2 decimal places. Use numpy np.round, then calculation and add the number of decimals at the end
-tips['Tip and Bill'] = np.round(tips['total_bill'] + tips['tip'],2)
-tips['Tip_Percentage'] = np.round(100 * tips['tip'] / tips['total_bill'],3)
-print(tips.head())
-
-#Dropping a column is done with .drop() DO NOT forget axis. 0 is Row, 1 is column
-#NB Remember it is possible that you drop a column but didn't make it permenant to the column
-print(tips.drop('Tip and Bill', axis = 1)) # Not Permenant
-tips = tips.drop('Tip and Bill', axis = 1) # Permenant
-print(tips)
-
-'''ROWS'''
-#Changing Index(Unique Identifier/Primary Key)
-#print(df.index())
-print(tips.index)
-print(tips.set_index("Payment ID"))
-
-#Make it Permanent
-tips = tips.set_index('Payment ID')
-
-print(tips.head)
-
-#Reset the Index
-tips = tips.reset_index()
-print(tips.head)
+##Getting all Orders made on the Weekend including Friday (Short Version)
+options = ['Fri', 'Sat', 'Sun']
+print(tips[tips['day'].isin(options)])
