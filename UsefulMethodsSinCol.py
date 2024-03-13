9ba2e895c5cafef7b11b186f206d697f50e4f201
SunnyShilongo <112655746+SunnyShilongo@users.noreply.github.com>
2024-03-14 01:23:38 +0200

    .apply
    lambda --> good for once off expression you don't plan on using a lot
    
    np.vectorize --> To run a specific function on a portion of your columns with th eaid of a function

--- UsefulMethodsSinCol.py
+++ UsefulMethodsSinCol.py
@@ -31,3 +31,39 @@ def yelp(price):
 #Making a table for the $ called Yelp
 tips['yelp'] = tips['total_bill'].apply(yelp)
 print(tips)
+
+''''LAMBDA EXPRESSIONS
+What is a lambda expression?
+It is good for once off expression you don't plan on using a lot'''
+
+def simple(num):
+    return num * 2
+#For lambda change teh def to
+
+print(simple(2))
+
+lambda num: num*2
+
+
+print(tips['total_bill'].apply(lambda num: num*2))
+
+
+
+def quality(total_bill, tip):
+    if tip / total_bill > 0.25:
+        return "Generous"
+    else:
+        return "Other"
+
+print(quality(16.99,1.01))
+#Select columns "tips[['total_bill', 'tip']]"
+#Call Lambda on the dataFrame being passed in "lambda tips"
+#Into the function we pass in the columns
+print(tips[['total_bill', 'tip']].apply(lambda tips: quality(tips['total_bill'],tips['tip']), axis=1))
+
+#assign thos to column
+#tips['Quality'] = tips[['total_bill', 'tip']].apply(lambda tips: quality(tips['total_bill'],tips['tip']), axis=1)
+print(tips)
+
+tips['Quality'] = np.vectorize(quality)(tips['total_bill'], tips['tip'])
+print(tips)
